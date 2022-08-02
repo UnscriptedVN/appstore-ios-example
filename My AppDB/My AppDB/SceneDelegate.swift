f999c8edaecbe8400e6bac8f9c281b96ba4433e8
Marquis Kurt <software@marquiskurt.net>
2022-08-02 12:12:16 -0400

    :tada: Initial commit
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

---
+++ My AppDB/My AppDB/SceneDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  SceneDelegate.swift
+//  My AppDB
+//
+//  Created by Marquis Kurt on 8/2/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: scene)
+        let mainVC = ViewController()
+        let navigationVC = UINavigationController(rootViewController: mainVC)
+        navigationVC.navigationBar.prefersLargeTitles = true
+        self.window?.rootViewController = navigationVC
+        self.window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {}
+
+    func sceneDidBecomeActive(_ scene: UIScene) {}
+
+    func sceneWillResignActive(_ scene: UIScene) {}
+
+    func sceneWillEnterForeground(_ scene: UIScene) {}
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,13 @@
+# Candella AppDB API Example
+
+This repository contains an example project using the Candella AppDB
+API to fetch Candella projects and store them in a Core Data database.
+
+## Getting Started
+
+TBD
+
+## License
+
+This example is licensed under the Mozilla Public License, v2.0. You can
+read your rights in the LICENSE file provided.
